a9aa5166dd7c021b7b788ef47a92721eaee7fe5a
2bo <developer.2bo@gmail.com>
2025-05-06 18:15:41 +0900

    定数の整理と不要なコメントの削除

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -4,12 +4,3 @@ export const CURSOR_URL = 'https://www.cursor.com/changelog';
 export const WINDSURF_URL = 'https://windsurf.com/editor/releases';
 export const CLINE_URL = 'https://github.com/cline/cline/releases';
 export const ROO_URL = 'https://github.com/RooVetGit/Roo-Code/releases';
-
-// Slack通知設定
-export const SLACK_WEBHOOK_ENV = 'SLACK_WEBHOOK';
-
-// CSS Selectors for specific content
-export const DEVIN_SELECTOR = '.release-notes-container';
-export const CURSOR_SELECTOR = '.changelog-entries';
-export const WINDSURF_SELECTOR = '.releases-list';
-export const GITHUB_RELEASES_SELECTOR = '.release-entry'; // 共通のGitHubリリースセレクタ
